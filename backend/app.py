58099d38b2c1888a6fd3c451891afdb1b3c4f5b9
MrAgeo <sergiojurado95@gmail.com>
2022-02-20 16:34:43 -0500

    Added '/time' route as an example to connect React Native with Flask.

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,19 @@
+import time
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'Hello World from Flask!'
+
+
+@app.route('/time')
+def get_current_time():  # put application's code here
+    return {"time": time.time()}
 
 
 if __name__ == '__main__':
