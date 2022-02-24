57fb9d41370256a3439d4bfa91b60bfbfeca742a
MrAgeo <sergiojurado95@gmail.com>
2022-02-24 10:21:29 -0500

    Added custom GUI Component Lib 'MiniComponents'

---
+++ frontend/lib/components/Minicomponents.js
@@ -0,0 +1,7 @@
+import {View} from "react-native";
+import styles from "./css/minicomponents.sass";
+import React from "react";
+
+const Separator = () => <View style={styles.separator}/>;
+
+export {Separator};
