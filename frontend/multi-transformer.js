89796683b86620b43b3e7dfebe4b0ed95a2c4c64
MrAgeo <sergiojurado95@gmail.com>
2022-03-08 11:56:24 -0500

    Added multi-transformer for file extensions:
    - Sass files (.sass & .scss)
    - SVG files (.svg)
    - Source files related with Metro

---
+++ frontend/multi-transformer.js
@@ -0,0 +1,18 @@
+/**
+ * Babel multi transformer for code, Sass files & SVG files
+ */
+
+// For React Native version 0.59 or later
+var upstreamTransformer = require("metro-react-native-babel-transformer");
+var sassTransformer = require("react-native-sass-transformer");
+var svgTransformer = require("react-native-svg-transformer");
+
+module.exports.transform = function({ src, filename, options }) {
+  if (filename.endsWith(".scss") || filename.endsWith(".sass")) {
+    return sassTransformer.transform({ src, filename, options });
+  } else if (filename.endsWith(".svg")) {
+    return svgTransformer.transform({ src, filename, options });
+  }  else {
+    return upstreamTransformer.transform({ src, filename, options });
+  }
+};
