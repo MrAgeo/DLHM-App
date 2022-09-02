59eb3568149e500c9e8813cd56ba0725b25872f2
MrAgeo <sergiojurado95@gmail.com>
2022-09-02 00:57:26 -0500

    Deleted unused import

--- frontend/src/features/ui/mini-components/mini-components.js
+++ frontend/src/features/ui/mini-components/mini-components.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text as Txt, TextInput as TxtInput, TouchableOpacity, StyleSheet } from "react-native";
 import { maxFontSize } from '../../../config';
 
